c83fc521fc7b2e93393a2a4fa9cebc19681d385b
rollean <95875549@qq.com>
2018-08-08 17:57:16 +0800

    123123

---
+++ src/main/java/com/example/groovydemo/GroovydemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.groovydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovydemoApplication.class, args);
+	}
+}
