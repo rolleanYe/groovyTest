cbe3377b6c4474d5a310e34b66fe420131412eba
rollean <95875549@qq.com>
2018-08-08 19:18:57 +0800

    123123123

--- src/main/java/com/example/groovydemo/GroovydemoApplication.java
+++ src/main/java/com/example/groovydemo/GroovydemoApplication.java
@@ -2,11 +2,14 @@ package com.example.groovydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GroovydemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GroovydemoApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(GroovydemoApplication.class, args);
+
 	}
+
 }
---
+++ src/main/java/com/example/groovydemo/TestImplApplicationRunner.java
@@ -0,0 +1,23 @@
+package com.example.groovydemo;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by rollean.
+ */
+
+@Component
+public class TestImplApplicationRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("1=============这个是测试ApplicationRunner接口=============");
+        System.out.println(args.getOptionNames());
+        System.out.println(args.getOptionValues("aaa"));
+        System.out.println(args.getOptionValues("bb"));
+
+        System.out.println("2=============这个是测试ApplicationRunner接口=============");
+    }
+
+}
