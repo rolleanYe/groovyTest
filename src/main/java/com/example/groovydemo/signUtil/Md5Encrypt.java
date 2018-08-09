1f2d1b9ac7d853123c0be06d47988f32865def4d
rollean <95875549@qq.com>
2018-08-09 14:37:36 +0800

    删除没用的类

--- src/main/java/com/example/groovydemo/signUtil/Md5Encrypt.java
+++ src/main/java/com/example/groovydemo/signUtil/Md5Encrypt.java
@@ -7,37 +7,20 @@ import java.security.SignatureException;
 
 
 /**
-* 功能：支付宝MD5签名处理核心文件，不需要修改
-* 版本：3.1
-* 修改日期：2010-11-01
-* 说明：
-* 以下代码只是为了方便商户测试而提供的样例代码，商户可以根据自己网站的需要，按照技术文档编写,并非一定要使用该代码。
-* 该代码仅供学习和研究支付宝接口使用，只是提供一个
+* 功能：MD5签名处理核心文件，不需要修改
 * */
 
 public class Md5Encrypt {
 
     /**
      * 对字符串进行MD5签名
-     *
-     * @param text
-     *            明文
-     *
-     * @return 密文
      */
     public static String md5(String text,String inputCharset) {
-
         return DigestUtils.md5Hex(getContentBytes(text, inputCharset));
-
     }
-    
 
     /**
      * 签名字符串
-     * @param text 需要签名的字符串
-     * @param key 密钥
-     * @param input_charset 编码格式
-     * @return 签名结果
      */
     public static String sign(String text, String key,String inputCharset) {
     	text = text + key;
@@ -45,12 +28,7 @@ public class Md5Encrypt {
     }
     
     /**
-     * 签名字符串
-     * @param text 需要签名的字符串
-     * @param sign 签名结果
-     * @param key 密钥
-     * @param input_charset 编码格式
-     * @return 签名结果
+     * 验证签名
      */
     public static boolean verify(String text, String sign, String key, String input_charset) {
     	text = text + key;
@@ -62,18 +40,11 @@ public class Md5Encrypt {
     		return false;
     	}
     }
-    /**
-     * @param content
-     * @param charset
-     * @return
-     * @throws SignatureException
-     * @throws UnsupportedEncodingException
-     */
+
     private static byte[] getContentBytes(String content, String charset) {
         if (charset == null || "".equals(charset)) {
             return content.getBytes();
         }
-
         try {
             return content.getBytes(charset);
         } catch (UnsupportedEncodingException e) {
